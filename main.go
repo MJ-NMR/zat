f71a638e5d85ad8b0cd0f2c253d49b48b071af69
zaater <almogtbanmr123@gmail.com>
2025-08-26 21:16:20 +0300

    Adam

---
+++ main.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	"log"
+
+	tea "github.com/charmbracelet/bubbletea"
+)
+
+type model struct {
+	name string
+	width int
+	height int
+}
+
+
+func (m model) Init() tea.Cmd {
+	return nil
+}	
+
+func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
+	switch msg := msg.(type) {
+	
+	case tea.WindowSizeMsg:
+		m.height = msg.Height
+		m.width = msg.Height
+		return m, nil
+
+	case tea.KeyMsg:
+		switch msg.String() {
+
+		case "ctrl+c", "q":
+            return m, tea.Quit
+		}
+
+	}
+	return m, nil
+}
+
+func (m model) View() string {
+	if m.width == 0 {
+		return "Zzz"
+	}
+
+	return "hello"
+}
+
+func main() {
+	app := tea.NewProgram(model{},tea.WithAltScreen())
+	if _, err := app.Run(); err != nil {
+		log.Panic(err)
+	}
+}
